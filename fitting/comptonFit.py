a4e39594ae36afb2b563f8c195e204c072a372db
FannyPack123 <philipp.zitzmann@t-online.de>
2024-08-13 14:27:28 +0200

    half gauss fitting works
    now working on finding turning point values

--- fitting/comptonFit.py
+++ fitting/comptonFit.py
@@ -23,6 +23,8 @@ d = 0.0
 
 pzero = np.array([Ec,sigma,a,b,c,d])
 
+p0 = np.array([500,190000,30])
+
 def findEdge(spectrum,sampleRate,cutoff):   #algorithm to find comptomn edges by looking for jumps in data
     
     edgeX = np.array([spectrum[0][0]])
@@ -52,6 +54,18 @@ def findEdge(spectrum,sampleRate,cutoff):   #algorithm to find comptomn edges by
 
 
 
+def halfGauss(EArr,mu,sigma,a,d):               #this one works!!!!!!!
+    
+    results = np.array([])
+    
+    for E in EArr:
+        if E < mu:
+            results = np.append(results, 0.0)
+        else:
+            results = np.append(results, (a/(sigma*math.sqrt(2*math.pi)))*np.exp(-0.5*((E-mu)/sigma)**2)+d)
+    
+    return results
+
 def CEFunc(E,Ec,sigma,a,b,c,d):     #theoretical compton edge function
     
     # if a <=0:
@@ -68,14 +82,6 @@ def logFunc(E,Ec,sigma,a,b,c,d):
     return np.log(CEFunc(E, Ec, sigma, a, b, c, d))
 
 
-
-
-# xVals = np.linspace(-b/(2*a)-Ec/2, Ec+10000, num = 1000)
-
-# yVals = CEFunc(xVals, *pzero)
-
-
-# spect = np.genfromtxt('../spectraOutput/06/Bi207/1300V_Offset0000_spect.csv', delimiter=',')    #read in data
 
 def getCEdgeVals(voltage,src,offset):
     for ch in ["00","01","02","03","04","05","06","07","08","09","10"]:
@@ -101,113 +107,37 @@ def getCEdgeVals(voltage,src,offset):
         print(CEs[0])
         outer = 1000
         
-        # plt.figure()
-        # plt.plot(spect[0],spect[1])
-        # plt.yscale('log')
-        
+
         for i in range(len(CEs[0])):     #cycle through all comptom edges and fit data to CE function
         
-            fitStart = int(list(spect[0]).index(CEs[0][i]) - outer*(1+i*.01))
-            fitEnd = int(list(spect[0]).index(CEs[0][i])  + outer*(1+i*.01))
-            # fitStart = int(CEs[0][i])  - outer
-            # fitEnd = int(CEs[0][i])  + outer
+            # fitStart = int(list(spect[0]).index(CEs[0][i]) - outer*(1+i*.01))
+            # fitEnd = int(list(spect[0]).index(CEs[0][i])  + outer*(1+i*.01))
+            fitStart = int(CEs[0][i])  - outer
+            fitEnd = int(CEs[0][i])  + outer
             if fitStart < 0:
                 continue
             print(fitStart)
             print(fitEnd)
             
             plt.figure()
-            plt.plot(spect[0]-CEs[0][i],spect[1],color='blue')
+            plt.title("channel no." + ch + ", " + src + ", " + voltage)
+            plt.plot(spect[0],spect[1],color='cyan')
             plt.yscale('log')
-            # plt.vlines(CEs[0], 0.0, max(spect[1]), color="purple", linestyles='dotted', label= "guesses")
-            plt.vlines(CEs[0]-CEs[0][i], 0.0, max(spect[1]), color="purple", linestyles='dotted', label= "guesses")
-
-            CE_params = optimize.curve_fit(CEFunc, spect[0][fitStart:fitEnd], spect[1][fitStart:fitEnd], np.append(CEs[0][i], pzero[1:]), bounds=((0, 0, 0, 0, 0, 0), (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)))
-            # CE_params = optimize.curve_fit(CEFunc, spect[0][fitStart:fitEnd]-CEs[0][i], spect[1][fitStart:fitEnd], pzero, bounds=((0, 0, 0, 0, 0, 0), (np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)))
-            
-            # CE_params = optimize.curve_fit(logFunc, spect[0][fitStart:fitEnd], spect[1][fitStart:fitEnd], np.append(CEs[0][0], pzero[1:]))
-            
+            plt.vlines(CEs[0], 0.0, max(spect[1]), color="purple", linestyles='dotted', label= "guesses")
+            # print(CEs[0][i])
+            CE_params = optimize.curve_fit(halfGauss, spect[0][fitStart:fitEnd], spect[1][fitStart:fitEnd], np.append((CEs[0][i]-p0[0]*1.177), p0))
             
             print(CE_params[0])
-            # print(CE_params[0][0]-CEs[0][1])
             
             
-            CE = CEFunc(spect[0], *CE_params[0])
-            # CE = logFunc(spect[0], *CE_params[0])
+            CE = halfGauss(spect[0], *CE_params[0])
+            
+            plt.hlines(1000, xmin=spect[0][fitStart:fitEnd][0], xmax=spect[0][fitStart:fitEnd][-1], color='red')
+
+            plt.plot(spect[0][fitStart:fitEnd], CE[fitStart:fitEnd],color="green")
+            plt.vlines(CE_params[0][0]+CE_params[0][1]*1.177, ymin=0, ymax=max(spect[1]), color='orange', label= "fitted Compton edge")
             
             
-            # plt.plot(spect[0][fitStart-outer:fitEnd+outer],spect[1][fitStart-outer:fitEnd+outer])
-            # plt.vlines(CEs[0], 0.0, max(spect[1]), color="purple")
-            # plt.plot(spect[0][fitStart:fitEnd], CE[fitStart:fitEnd])
-            # plt.hlines(1000, xmin=spect[0][fitStart:fitEnd][0]-CEs[0][i], xmax=spect[0][fitStart:fitEnd][-1]-CEs[0][i], color='red')
-            plt.hlines(1000, xmin=spect[0][fitStart:fitEnd][0]-CEs[0][i], xmax=spect[0][fitStart:fitEnd][-1]-CEs[0][i], color='red')
-            
-            plt.plot(spect[0][fitStart:fitEnd]-CEs[0][i], CE[fitStart:fitEnd])
-            # plt.plot(spect[0]-CEs[0][i], CE)
-            # plt.plot(spect[0], CE)
-            # plt.vlines(CE_params[0][0], ymin=0, ymax=max(spect[1]), color='orange', label= "fitted Compton edge")
-            plt.vlines(CE_params[0][0]-CEs[0][i], ymin=0, ymax=max(spect[1]), color='orange', label= "fitted Compton edge")
-            
-            
-            # fit2Start = 3500
-            # fit2End = 7000
-            
-            # CE2_params = optimize.curve_fit(CEFunc, spect[0][fit2Start:fit2End], spect[1][fit2Start:fit2End], np.append(CEs[0][1]+1000, pzero[1:]))
-            # print(CE2_params[0])
-            # print(CE2_params[0][0]-CEs[0][1])
-            
-            # print(params[0])
-            
-            # xVals = np.linspace(-b/(2*a)-Ec/2, Ec+1000, num = 1000)
-            
-            # CE2 = CEFunc(spect[0], *CE2_params[0])
-            
-            # yVals = CEFunc(spect[0], *pzero)
             plt.legend()
         # plt.savefig("testFitting.pdf", format="pdf", dpi=400)
         plt.show()
-        
-        
-        # print(xVals)
-        # print(yVals)
-        
-        # print(spect)
-        
-        
-        
-        # plt.figure()
-        # plt.yscale('log')
-        # plt.plot(spect[0][fit1Start-outer:fit1End+outer],spect[1][fit1Start-outer:fit1End+outer])
-        # plt.plot(spect[0][fit1Start:fit1End], CE1[fit1Start:fit1End])
-        # plt.vlines(CE1_params[0][0], ymin=0, ymax=max(spect[1]), color='orange', linestyles='dotted')
-        # plt.show()
-        
-        # plt.figure()
-        # plt.yscale('log')
-        # plt.plot(spect[0][fit2Start-outer:fit2End+outer],spect[1][fit2Start-outer:fit2End+outer])
-        # plt.plot(spect[0][fit2Start:fit2End], CE2[fit2Start:fit2End])
-        # plt.vlines(CE2_params[0][0], ymin=0, ymax=max(spect[1]), color='orange', linestyles='dotted')
-        # plt.show()
-        
-        
-        # plt.figure()
-        # plt.yscale('log')
-        # plt.plot(spect[0],spect[1])
-        
-        
-        # plt.plot(spect[0][fit1Start:fit1End], CE1[fit1Start:fit1End])
-        # plt.vlines(CE1_params[0][0], ymin=0, ymax=max(spect[1]), color='red', linestyles='dotted')
-        # plt.plot(spect[0][fit2Start:fit2End], CE2[fit2Start:fit2End])
-        # plt.vlines(CE2_params[0][0], ymin=0, ymax=max(spect[1]), color='red', linestyles='dotted')
-        
-        
-        # plt.vlines(CEs[0], 0.0, max(spect[1]), color="purple")
-        # plt.plot(CEs[1],CEs[2])
-        
-        # # plt.figure()
-        # # plt.plot(xVals, yVals)
-        # # plt.vlines(Ec, ymin=0, ymax=a*Ec**2+b*Ec+c, color='orange', linestyles='dotted')
-
-
-
-# plt.show()
--- fitting/gaussFit.py
+++ fitting/gaussFit.py
@@ -16,7 +16,7 @@ def gauss(x,sigma,mu):
     return (1/(sigma*math.sqrt(2*math.pi)))*np.exp(-0.5*((x-mu)/sigma)**2)
 
 
-def halfGauss(xArr,sigma,mu,a,d):
+def halfGauss(xArr,sigma,mu,a,d):               #this one works!!!!!!!
     
     results = np.array([])
     
@@ -53,17 +53,18 @@ yVals = gauss(xVals, *p0)
 
 #fit to data
 
-popt, pcov = optimize.curve_fit(gauss, np.linspace(0.0, 50.0, num = 41), yVals, p0)
+popt, pcov = optimize.curve_fit(halfGauss, np.linspace(-50.0, 50.0, num = 41), yVals, pzero)
 # print(*popt)
 
-popt2, pcov2 = optimize.curve_fit(halfGauss, spectrum[0][2000:4500],spectrum[1][1500:4000], [3700,3250,50,50])
+popt2, pcov2 = optimize.curve_fit(halfGauss, spectrum[0][2000:4500],spectrum[1][1500:4000], [500,3250,190000,30])
 print(*popt2)
 
 xFit = np.linspace(-50.0, 50.0, num = 201)
-yFit = gauss(xFit,*popt)
+yFit = halfGauss(xFit,*popt)
 
 
 yFit2 = halfGauss(spectrum[0][2000:4500],*popt2)
+yFit2prime = halfGauss(spectrum[0][2000:4500],500,3250,190000,30)
 
 # print(xVals)
 # print(yFit)
@@ -72,6 +73,7 @@ yFit2 = halfGauss(spectrum[0][2000:4500],*popt2)
 #plot
 
 plt.figure()
+plt.title("fit1")
 # plt.vlines([sigma,sigma*1.177], min(yVals), [gauss(sigma,sigma,mu),max(yVals)], color=['orange','red'], linestyles='dashed')
 plt.plot(xVals,yVals,'.')
 plt.plot(xFit, yFit, color = 'green')
@@ -79,9 +81,12 @@ plt.vlines(popt[0]*1.177, min(yFit), max(yFit), color='red', linestyles='dashed'
 
 
 plt.figure()
+plt.title("fit2")
 # plt.yscale('log')
 plt.plot(spectrum[0][2000:4500],spectrum[1][1500:4000])
 plt.plot(spectrum[0][2000:4500],yFit2)
+plt.plot(spectrum[0][2000:4500],yFit2prime)
+plt.vlines(popt2[1]+popt2[0]*1.177,ymin=0.0, ymax=max(spectrum[1][1500:4000]),color="red",linestyles="dashed")
 plt.show()
 
 
